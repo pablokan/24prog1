225f838f78f721de8d4152681e06e0372eab4599
Pablo Kaniefsky <pablokan@gmail.com>
2024-09-04 08:39:28 -0300

    pre parcial2 m2

--- parciales/parcial2/parcial2_M2_2024.py
+++ parciales/parcial2/parcial2_M2_2024.py
@@ -2,7 +2,7 @@ class Video:
     def __init__(self, titulo, vistas, actores):
         self.titulo = titulo
         self.vistas = vistas
-        self.actores = actores.split(',')
+        self.actores = actores
    
 class Serie(Video):
     def __init__(self, titulo, vistas, actores, temporadas):
@@ -16,33 +16,34 @@ class Pelicula(Video):
 
 class Plataforma:
     def __init__(self, series, peliculas):
-        self.series = series
-        self.peliculas = peliculas
+        self.series = [Serie(*serie) for serie in series]
+        self.peliculas = [Pelicula(*pelicula) for pelicula in peliculas]
 
     def videoMasVisto(self):
         todosLosVideos = self.series + self.peliculas
-        l = [(v[1], v[0]) for v in todosLosVideos]
-        l.sort(reverse=True)
-        return l[0][1]
+        vistasYvideo = [(v.vistas, v.titulo) for v in todosLosVideos]
+        vistasYvideo.sort(reverse=True) # ordenar por cantidad de vistas
+        return vistasYvideo[0][1]
 
-    def actoresEnAmbas(self):
-        
+    def actoresEnAmbas(self) -> set:
+        actoresSeries = ''
+        for serie in self.series:
+            actoresSeries += serie.actores
+        actoresSeries = set(actoresSeries.split(','))
+        actoresPeliculas = ''
+        for pelicula in self.peliculas:
+            actoresPeliculas += pelicula.actores
+        actoresPeliculas = set(actoresPeliculas.split(','))
+        return actoresSeries.intersection(actoresPeliculas)
 
     def seriesMenos5Temporadas(self):
-        return [serie for serie in self.series if serie.temporadas < 5]
+        return [serie.titulo for serie in self.series if serie.temporadas < 5]
 
 
 plataforma = Plataforma([["Peaky Blinders", 1234567, 'Cillian Murphy,Paul Anderson,Helen McCrory', 6], ["The Umbrella Academy", 2434908, 'Tom Hopper,Emmy Raver-Lampman,Ellen Page,David Castañeda', 2]], [["Inmortales", 35, 'Mirtha Legrand,Carlitos Balá,Elizabeth The Second', 30],["Inception", 17319533, 'Leonardo DiCaprio,Ellen Page,Joseph Gordon-Levitt', 148], ["Batman Begins",4760183, 'Christian Bale,Leonardo DiCaprio,Cillian Murphy,Michael Caine', 140]])
 
-# plataforma.actoresEnAmbas()
-# plataforma.seriesMenos5Temporadas()
+
 
 print(f"El video más visto: {plataforma.videoMasVisto()}")
-
-      
-# print("Actores que trabajan en series y películas:")
-# for actor in actores_en_ambos:
-#     print(f"- {actor}")
-# print("Series que tienen menos de 5 temporadas:")
-# for serie in series_menos_cinco_temporadas:
-#     print(f"- {serie.titulo}")
+print(f"Actores que trabajan en series y películas: {plataforma.actoresEnAmbas()}")
+print(f"Series que tienen menos de 5 temporadas: {plataforma.seriesMenos5Temporadas()}")
